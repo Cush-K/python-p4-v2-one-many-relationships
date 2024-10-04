3193f0db058628cc4d88c5e8546fb01a46a9f588
Kalvine <kkalvine4@gmail.com>
2024-10-05 00:15:47 +0300

    One-to-many r/ships with flask

--- server/models.py
+++ server/models.py
@@ -2,6 +2,7 @@
 
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from sqlalchemy_serializer import SerializerMixin as sm
 
 metadata = MetaData(
     naming_convention={
@@ -12,34 +13,46 @@ metadata = MetaData(
 db = SQLAlchemy(metadata=metadata)
 
 
-class Employee(db.Model):
+class Employee(db.Model, sm):
     __tablename__ = "employees"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     hire_date = db.Column(db.Date)
+    
+    onboarding = db.relationship("Onboarding", uselist=False, back_populates = 'employee', cascade='all, delete-orphan')
+    reviews = db.relationship("Review", back_populates = 'employee', cascade='all, delete-orphan')
+    
 
     def __repr__(self):
         return f"<Employee {self.id}, {self.name}, {self.hire_date}>"
 
 
-class Onboarding(db.Model):
+class Onboarding(db.Model, sm):
     __tablename__ = "onboardings"
 
     id = db.Column(db.Integer, primary_key=True)
     orientation = db.Column(db.DateTime)
     forms_complete = db.Column(db.Boolean, default=False)
+    employee_id = db.Column(db.Integer, db.ForeignKey("employees.id"))
+    
+    employee = db.relationship("Employee", back_populates = 'onboarding')
+    
 
     def __repr__(self):
         return f"<Onboarding {self.id}, {self.orientation}, {self.forms_complete}>"
 
 
-class Review(db.Model):
+class Review(db.Model, sm):
     __tablename__ = "reviews"
 
     id = db.Column(db.Integer, primary_key=True)
     year = db.Column(db.Integer)
     summary = db.Column(db.String)
+    employee_id = db.Column(db.Integer, db.ForeignKey("employees.id"))
+    
+    employee = db.relationship("Employee", back_populates = 'reviews')
+
 
     def __repr__(self):
         return f"<Review {self.id}, {self.year}, {self.summary}>"
